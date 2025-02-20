9b4bc9708b864ed673e93d61938137225f94e9d2
lewohy <lwh8762@gmail.com>
2025-02-20 22:18:36 +0900

    chore: typescaleStyles 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,14 @@ import { App } from '@/app';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import 'pretendard/dist/web/variable/pretendardvariable.css';
+import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js';
 
 const root = document.getElementById('root');
+
+if (typescaleStyles.styleSheet) {
+    document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
+}
 
 if (!root) {
     throw new Error('No root element found');
