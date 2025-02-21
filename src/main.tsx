8c911517a98029f4ab9942a243f0bfae491ce462
lewohy <lwh8762@gmail.com>
2025-02-21 16:13:05 +0900

    chore: reset css 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,15 @@ import { styles as typescaleStyles } from '@material/web/typography/md-typescale
 import 'material-symbols';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import resetCss from 'reset-css/reset.css?inline';
 import './index.scss';
 
+// reset css 적용
+const cssStyleSheet = new CSSStyleSheet();
+cssStyleSheet.replaceSync(`@layer {\n ${resetCss} \n} `);
+document.adoptedStyleSheets.push(cssStyleSheet);
+
+// material typescale css 적용
 if (typescaleStyles.styleSheet) {
     document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
 }
