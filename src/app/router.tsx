5213a592f6cc9155ffd1516aca7a3d348f1d48f9
lewohy <lwh8762@gmail.com>
2025-02-22 15:19:41 +0900

    refactor: import 및 material component 구조 수정

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,7 @@
 import { paths } from '@/config/paths';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { Test } from './routes/test';
-import { Signin } from './routes/login';
+import { Signin } from './routes/signin';
 
 export const AppRouter = () => {
     return (
