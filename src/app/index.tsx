e20a134c0d519e28ee2d52df41f882ec226b2ee9
lewohy <lwh8762@gmail.com>
2025-02-23 15:54:45 +0900

    chore: dark mode를 hook으로 감시하도록 변경

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { applyTheme, argbFromHex, themeFromSourceColor } from '@material/material-color-utilities';
 import { useEffect, useMemo } from 'react';
+import { useMediaQuery } from 'usehooks-ts';
 import { AppRouter } from './router';
 
 export const App = () => {
@@ -7,8 +8,7 @@ export const App = () => {
     const theme = useMemo(() => themeFromSourceColor(argbFromHex('#65558F')), []);
 
     // 사용자가 다크모드를 활성화했는지 확인
-    // TODO: 매 렌더링마다 확인하지 말고, 실제로 값이 변경될 떄만 확인할수있나?
-    const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const systemDark = useMediaQuery('(prefers-color-scheme: dark)');
 
     useEffect(() => {
         // body에 material tokens에 관한 custom properties를 업데이트하여 테마 적용
